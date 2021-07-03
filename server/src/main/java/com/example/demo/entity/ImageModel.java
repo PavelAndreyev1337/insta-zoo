f841ba77111193c9f8f280e7d3f4a278af99517e
PavelAndreyev1337 <pavel.andreyev.98@gmail.com>
2021-07-03 15:23:17 +0300

    added image model

---
+++ server/src/main/java/com/example/demo/entity/ImageModel.java
@@ -0,0 +1,10 @@
+package com.example.demo.entity;
+
+public class ImageModel {
+
+    private Long id;
+    private String name;
+    private byte[] imageBytes;
+    private Long userId;
+    private Long postId;
+}
