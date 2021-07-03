f775f2d53f77b7ab937724f7299b1846322939ae
PavelAndreyev1337 <pavel.andreyev.98@gmail.com>
2021-07-03 16:44:14 +0300

    added repository for image model

---
+++ server/src/main/java/com/example/demo/repository/ImageRepository.java
@@ -0,0 +1,15 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.ImageModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ImageRepository extends JpaRepository<ImageModel, Long> {
+
+    Optional<ImageModel> findByUserId(Long id);
+
+    Optional<ImageModel> findByPostId(Long id);
+}
