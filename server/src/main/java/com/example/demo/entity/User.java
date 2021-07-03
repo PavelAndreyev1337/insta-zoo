5775f311c3f92d30fc1850c5a4bd1b3d29febc21
PavelAndreyev1337 <pavel.andreyev.98@gmail.com>
2021-07-03 15:42:41 +0300

    added annotations for user model

--- server/src/main/java/com/example/demo/entity/User.java
+++ server/src/main/java/com/example/demo/entity/User.java
@@ -1,26 +1,46 @@
 package com.example.demo.entity;
 
 import com.example.demo.entity.enums.ERole;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
 
-import javax.persistence.PrePersist;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+@Data
+@Entity
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String name;
+    @Column(unique = true, updatable = false)
     private String username;
+    @Column(nullable = false)
     private String lastname;
+    @Column(unique = true)
     private String email;
+    @Column(columnDefinition = "text")
     private String bio;
+    @Column(length = 3000)
     private String password;
 
+    @ElementCollection(targetClass = ERole.class)
+    @CollectionTable(name="user_role",
+    joinColumns = @JoinColumn("user_id"))
     private Set<ERole> role = new HashSet<>();
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "user", orphanRemoval = true)
     private List<Post> posts = new ArrayList<>();
+
+    @JsonFormat(pattern = "yyyy-mm-dd HH:mm:ss")
+    @Column(updatable = false)
     private LocalDateTime createdDate;
 
     @PrePersist
