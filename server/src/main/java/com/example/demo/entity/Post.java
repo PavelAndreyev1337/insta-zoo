f00ff335d34fef08ac7a53cda5896444e7335a74
PavelAndreyev1337 <pavel.andreyev.98@gmail.com>
2021-07-03 15:56:11 +0300

    added annotations for post

--- server/src/main/java/com/example/demo/entity/Post.java
+++ server/src/main/java/com/example/demo/entity/Post.java
@@ -1,23 +1,34 @@
 package com.example.demo.entity;
 
-import javax.persistence.PrePersist;
+import lombok.Data;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+@Data
+@Entity
 public class Post {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String caption;
     private String location;
     private Integer likes;
 
+    @Column
+    @ElementCollection(targetClass = String.class)
     private Set<String> likedUsers = new HashSet<>();
+    @ManyToOne(fetch = FetchType.LAZY)
     private User user;
+    @OneToMany(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER, mappedBy = "post", orphanRemoval = true)
     private List<Comment> comments = new ArrayList<>();
+    @Column(updatable = false)
     private LocalDateTime createdDate;
 
     @PrePersist
