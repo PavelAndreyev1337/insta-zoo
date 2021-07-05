4a29a17aa71489534846780be3f7da6e276a5396
PavelAndreyev1337 <pavel.andreyev.98@gmail.com>
2021-07-05 16:08:45 +0300

    added error interceptor

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,6 +8,7 @@ import {MaterialModule} from "./material-module";
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {authInterceptorProviders} from "./helper/auth-interceptor.service";
+import {ErrorInterceptorService} from './helper/error-interceptor.service';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,7 @@ import {authInterceptorProviders} from "./helper/auth-interceptor.service";
     HttpClientModule,
     FormsModule
   ],
-  providers: [authInterceptorProviders],
+  providers: [authInterceptorProviders, ErrorInterceptorService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- client/src/app/helper/error-interceptor.service.ts
+++ client/src/app/helper/error-interceptor.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import {HTTP_INTERCEPTORS,HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {TokenStorageService} from '../service/token-storage.service';
+import {NotificationService} from '../service/notification.service';
+import { Observable, throwError } from 'rxjs';
+import {catchError} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ErrorInterceptorService {
+export class ErrorInterceptorService implements HttpInterceptor {
 
-  constructor() { }
+  constructor(private tokenService: TokenStorageService,
+    private notificationService: NotificationService) { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(req).pipe(catchError(err => {
+      if (err.status === 401) {
+        this.tokenService.logout();
+        window.location.reload()
+      }
+      const error = err.error.message || err.statusText;
+      this.notificationService.showSnackBar(error);
+      return throwError(error);
+    }))
+  }
 }
+
+export const authErrorInterceptorProvider = [
+  {provide: HTTP_INTERCEPTORS, userClass: ErrorInterceptorService, multi: true}
+];
