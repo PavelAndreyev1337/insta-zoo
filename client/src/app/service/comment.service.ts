006fdde08c7f205ffed6088e8ae5f2d34c25bfec
PavelAndreyev1337 <pavel.andreyev.98@gmail.com>
2021-07-05 15:42:52 +0300

    added comment service

--- client/src/app/service/comment.service.ts
+++ client/src/app/service/comment.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+const COMMENT_API = 'http://localhost:8080/api/image/';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommentService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  addToCommentToPost(postId: number, message: string): Observable<any> {
+    return this.http.post(COMMENT_API + postId + '/create', {
+      message: message
+    });
+  }
+
+  getCommentsToPost(postId: number): Observable<any> {
+    return this.http.get(COMMENT_API + postId + '/all');
+  }
+
+  delete(commentId: number): Observable<any> {
+    return this.http.post(COMMENT_API + commentId + '/delete', null);
+  }
 }
