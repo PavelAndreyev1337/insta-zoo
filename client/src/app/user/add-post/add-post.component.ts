a25591a5d0c79159b3d5fbdecf2e23ac06993696
PavelAndreyev1337 <pavel.andreyev.98@gmail.com>
2021-07-05 22:01:13 +0300

    added add-post component

--- client/src/app/user/add-post/add-post.component.ts
+++ client/src/app/user/add-post/add-post.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Post} from '../../models/Post';
+import {PostService} from '../../service/post.service';
+import {ImageUploadService} from '../../service/image-upload.service';
+import {NotificationService} from '../../service/notification.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-add-post',
@@ -7,9 +13,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddPostComponent implements OnInit {
 
-  constructor() { }
+  postForm: FormGroup;
+  selectedFile: File;
+  isPostCreated = false;
+  createdPost: Post;
+  previewImgURL: any;
 
-  ngOnInit(): void {
+  constructor(private postService: PostService,
+              private imageUploadService: ImageUploadService,
+              private notificationService: NotificationService,
+              private router: Router,
+              private fb: FormBuilder) {
   }
 
+  ngOnInit(): void {
+    this.postForm = this.createPostForm();
+  }
+
+  createPostForm(): FormGroup {
+    return this.fb.group({
+      title: ['', Validators.compose([Validators.required])],
+      caption: ['', Validators.compose([Validators.required])],
+      location: ['', Validators.compose([Validators.required])],
+    });
+  }
+
+  submit(): void {
+
+    this.postService.createPost({
+      title: this.postForm.value.title,
+      caption: this.postForm.value.caption,
+      location: this.postForm.value.location,
+    }).subscribe(data => {
+      this.createdPost = data;
+      console.log(data);
+
+      if (this.createdPost.id != null) {
+        this.imageUploadService.uploadImageToPost(this.selectedFile, this.createdPost.id)
+          .subscribe(() => {
+            this.notificationService.showSnackBar('Post created successfully');
+            this.isPostCreated = true;
+            this.router.navigate(['/profile']);
+          });
+      }
+    });
+  }
+
+  onFileSelected(event): void {
+    this.selectedFile = event.target.files[0];
+
+    const reader = new FileReader();
+    reader.readAsDataURL(this.selectedFile);
+    reader.onload = (e) => {
+      this.previewImgURL = reader.result;
+    };
+  }
 }
