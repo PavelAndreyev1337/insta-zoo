4f2ddacd39243c1b0f1f661770337daae6eb26a0
PavelAndreyev1337 <pavel.andreyev.98@gmail.com>
2021-07-05 21:34:26 +0300

    added edit user component

--- client/src/app/user/edit-user/edit-user.component.ts
+++ client/src/app/user/edit-user/edit-user.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { UserService } from '../../service/user.service';
+import { NotificationService } from '../../service/notification.service';
+import {User} from '../../models/User';
 
 @Component({
   selector: 'app-edit-user',
@@ -7,9 +12,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditUserComponent implements OnInit {
 
-  constructor() { }
+  public profileEditForm: FormGroup;
+
+  constructor(private dialogRef: MatDialogRef<EditUserComponent>,
+    private fb: FormBuilder,
+    private notificationService: NotificationService,
+    @Inject(MAT_DIALOG_DATA) public data,
+    private userService: UserService) { }
 
   ngOnInit(): void {
+    this.profileEditForm = this.createProfileForm();
   }
 
+  createProfileForm(): FormGroup {
+    return this.fb.group({
+      firstName: [
+        this.data.user.firstname,
+        Validators.compose([Validators.required])
+      ],
+      lastName: [
+        this.data.user.lastname,
+        Validators.compose([Validators.required])
+      ],
+      bio: [
+        this.data.user.bio,
+        Validators.compose([Validators.required])
+      ]
+    });
+  }
+
+  submit(): void {
+    this.userService.updateUser(this.updateUser()).subscribe(() => {
+      this.notificationService.showSnackBar('User updated successfully');
+      this.dialogRef.close()
+    })
+  }
+
+  private updateUser(): User {
+    this.data.user.firstname = this.profileEditForm.value.firstName;
+    this.data.user.lastname = this.profileEditForm.value.lastName;
+    this.data.user.bio = this.profileEditForm.value.bio;
+    return this.data.user;
+  }
+
+  closeDialog() {
+    this.dialogRef.close();
+  }
 }
