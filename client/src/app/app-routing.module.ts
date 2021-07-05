b132a18ddbbdce73075a2f7d8256af003bc9e800
PavelAndreyev1337 <pavel.andreyev.98@gmail.com>
2021-07-05 21:10:16 +0300

    added profile component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,11 +4,18 @@ import {LoginComponent} from "./auth/login/login.component";
 import { RegisterComponent } from './auth/register/register.component';
 import { IndexComponent } from './layout/index/index.component';
 import {AuthGuardService} from './helper/auth-guard.service';
+import {ProfileComponent} from './user/profile/profile.component';
+import { UserPostsComponent } from './user/user-posts/user-posts.component';
+import { AddPostComponent } from './user/add-post/add-post.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'main', component: IndexComponent, canActivate: [AuthGuardService]},
+  {path: 'profile', component: ProfileComponent, canActivate: [AuthGuardService], children: [
+    {path: '', component: UserPostsComponent, canActivate: [AuthGuardService]},
+    {path: 'add', component: AddPostComponent, canActivate: [AuthGuardService]}
+  ]},
   {path: '', redirectTo: 'main', pathMatch: 'full'}
 ];
 
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -13,6 +13,10 @@ import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { NavigationComponent } from './layout/navigation/navigation.component';
 import { IndexComponent } from './layout/index/index.component';
+import { ProfileComponent } from './user/profile/profile.component';
+import { UserPostsComponent } from './user/user-posts/user-posts.component';
+import { EditUserComponent } from './user/edit-user/edit-user.component';
+import { AddPostComponent } from './user/add-post/add-post.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +24,11 @@ import { IndexComponent } from './layout/index/index.component';
     LoginComponent,
     RegisterComponent,
     NavigationComponent,
-    IndexComponent
+    IndexComponent,
+    ProfileComponent,
+    UserPostsComponent,
+    EditUserComponent,
+    AddPostComponent
   ],
   imports: [
     BrowserModule,
