2b6c2c77f4eca895b0b2cb2e0297666432f51c14
PavelAndreyev1337 <pavel.andreyev.98@gmail.com>
2021-07-05 15:20:16 +0300

    added post service

--- client/src/app/service/post.service.ts
+++ client/src/app/service/post.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Post} from "../models/Post";
+import { Observable } from 'rxjs';
+
+const POST_API = 'http://localhost:8080/api/post/';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  createPost(post: Post): Observable<any> {
+    return this.http.post(POST_API + 'create', post);
+  }
+
+  getAllPosts(): Observable<any> {
+    return this.http.get(POST_API + 'all');
+  }
+
+  getPostForCurrentUser(): Observable<any> {
+    return this.http.get(POST_API + 'user/posts');
+  }
+
+  delete(id: number): Observable<any> {
+    return this.http.post(POST_API + id + '/delete', null);
+  }
+
+  likePost(id: number, username: string): Observable<any> {
+    return this.http.post(POST_API + id + '/' + username + '/like', null);
+  }
 }
