b651567eb8af33a78077ebf7151c271159701d4b
PavelAndreyev1337 <pavel.andreyev.98@gmail.com>
2021-07-05 21:45:22 +0300

    added user posts component

--- client/src/app/user/user-posts/user-posts.component.ts
+++ client/src/app/user/user-posts/user-posts.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Post} from '../../models/Post';
+import {PostService} from '../../service/post.service';
+import {ImageUploadService} from '../../service/image-upload.service';
+import {CommentService} from '../../service/comment.service';
+import {NotificationService} from '../../service/notification.service';
 
 @Component({
   selector: 'app-user-posts',
@@ -7,9 +12,72 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserPostsComponent implements OnInit {
 
-  constructor() { }
+  isUserPostsLoaded = false;
+  posts: Post [];
+
+  constructor(private postService: PostService,
+              private imageService: ImageUploadService,
+              private commentService: CommentService,
+              private notificationService: NotificationService) {
+  }
 
   ngOnInit(): void {
+    this.postService.getPostForCurrentUser()
+      .subscribe(data => {
+        console.log(data);
+        this.posts = data;
+        this.getImagesToPosts(this.posts);
+        this.getCommentsToPosts(this.posts);
+        this.isUserPostsLoaded = true;
+      });
+  }
+
+  getImagesToPosts(posts: Post[]): void {
+    posts.forEach(p => {
+      this.imageService.getImageToPost(p.id)
+        .subscribe(data => {
+          p.image = data.imageBytes;
+        });
+    });
+  }
+
+
+  getCommentsToPosts(posts: Post[]): void {
+    posts.forEach(p => {
+      this.commentService.getCommentsToPost(p.id)
+        .subscribe(data => {
+          p.comments = data;
+        });
+    });
+  }
+
+  removePost(post: Post, index: number): void {
+    console.log(post);
+    const result = confirm('Do you really want to delete this post?');
+    if (result) {
+      this.postService.delete(post.id)
+        .subscribe(() => {
+          this.posts.splice(index, 1);
+          this.notificationService.showSnackBar('Post deleted');
+        });
+    }
+  }
+
+  formatImage(img: any): any {
+    if (img == null) {
+      return null;
+    }
+    return 'data:image/jpeg;base64,' + img;
+  }
+
+  deleteComment(commentId: number, postIndex: number, commentIndex: number): void {
+    const post = this.posts[postIndex];
+
+    this.commentService.delete(commentId)
+      .subscribe(() => {
+        this.notificationService.showSnackBar('Comment removed');
+        post.comments.splice(commentIndex, 1);
+      });
   }
 
 }
